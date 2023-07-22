8da2befd9ce94e7e162adf1c903464d36f12ebc8
Ajay M <ajay@shivy.co.in>
2023-07-22 20:43:18 +0530

    Updated reddit script for db creation

--- src/reddit-account-creator.py
+++ src/reddit-account-creator.py
@@ -1,12 +1,11 @@
 import pickle
 from classes import FakeMailBox, FakePerson, Reddit
+from models import User, AccessToken
 from utils import get_logger
 import os
 
 logger = get_logger(__name__)
 USERS_COUNT = 1  # 100000
-DBC_USERNAME = os.getenv("DBC_USERNAME")
-DBC_PASSWORD = os.getenv("DBC_PASSWORD")
 
 created_user_count = 0
 while created_user_count < USERS_COUNT:
@@ -22,7 +21,30 @@ while created_user_count < USERS_COUNT:
     res = reddit_bot.create_account()
 
     if res:
-        with open(f"user-{person.username}", "wb") as f:
-            pickle.dump(person, f)
+        logger.info(f"Adding user {person.username} to database")
+        user = User(
+            username=person.username,
+            password=person.password,
+            create_account=datetime.now(),
+        )
+        user.save()
+        try:
+            # Generate token
+            token = reddit_bot.get_access_token()
+
+            user.last_used = datetime.now()
+
+            # Create new access token
+            access_token_obj = AccessToken(
+                username=user.username,
+                token=token,
+                created_at=datetime.now(),
+            )
+            access_token_obj.save()
+        except Exception as e:
+            logger.error(f"Failed to generate token for user {person.username}")
         created_user_count += 1
-    # Find a way to store this user globally
+
+    reddit_bot.close_me()
+
+    time.sleep(600)
